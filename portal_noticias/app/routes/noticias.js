b13649b51fab5ff3d28af1b02f2333724d77e54b
Joao Daniel Rufino <joaodaniel0405@gmail.com>
2018-06-16 23:06:43 -0300

    js

--- portal_noticias/app/routes/noticias.js
+++ portal_noticias/app/routes/noticias.js
@@ -1,5 +1,11 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app){
+  var connec = dbConnection();
+  
   app.get('/noticias', function(req, res){
-    res.render("noticias/noticias");
+    connec.query('select * from portal_noticias', function(erro, result){
+      res.render("noticias/noticias", {noticias: result})
+    });
   });
 }
---
+++ portal_noticias/config/dbConnection.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+
+module.exports = function(){
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodejs'
+  });
+}
