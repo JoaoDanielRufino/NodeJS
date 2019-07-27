b6b5f1541fc1d8dcd05a1c68167622618317b59f
Joao Daniel Rufino <joaodaniel0405@gmail.com>
2019-07-27 20:08:48 -0300

    ...

--- market/MongoDB/util/database.js
+++ market/MongoDB/util/database.js
@@ -1,8 +1,12 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
+  MongoClient.connect("mongodb://localhost:27017")
+    .then(client => {
+      callback(client);
+    })
+    .catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
